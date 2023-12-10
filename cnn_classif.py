93a32ec84797a3896a17e43a2c8d43bf1dd25828
Vishak Kamath <115456558+vxshxk@users.noreply.github.com>
2023-12-10 20:12:10 +0530

    Updated cnn_classif.py
    Tried to upload test dataset but files too big.

--- cnn_classif.py
+++ cnn_classif.py
@@ -11,7 +11,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #HyperParameters
 epochs=9
 batch_size = 8
-learning_rate = 0.01
+learning_rate = 0.005
 
 #transform
 transform = transforms.Compose(
@@ -73,3 +73,32 @@ for epoch in range(epochs):
         
         if (i+1) % 2000 == 0:
             print (f'Epoch [{epoch+1}/{epochs}], Step [{i+1}/{len(train_load)}], Loss: {loss.item():.4f}')
+
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in test_loadgit:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+        
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predicted[i]
+            if (label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Accuracy of {classes[i]}: {acc} %')
